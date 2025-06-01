9251a4532c20e1c386ac2d390cc955f7fd05a1a3
Ritav Das <ritavdas@gmail.com>
2025-06-02 00:38:26 +0530

    Update globals.css and layout.tsx for improved styling and consistency
    - Change background color in globals.css to enhance dark mode support
    - Comment out foreground color variables for clarity
    - Adjust body font-family to use CSS variable for better flexibility
    - Minor formatting change in layout.tsx for consistency

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={inter.className}>
-				<main className="min-h-screen bg-gray-50">{children}</main>
+				<main className="min-h-screen bg-gray-50 ">{children}</main>
 			</body>
 		</html>
 	);
