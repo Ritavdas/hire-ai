fa22e4fd56cfa9f1def7c3bf14caaedc928661be
Ritav Das <ritavdas@gmail.com>
2025-06-02 11:00:14 +0530

    Fix unused variable warning in AISummary component

--- app/components/AISummary.tsx
+++ app/components/AISummary.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { StarIcon, EyeIcon, ChevronLeftIcon } from "./icons";
 
 type SummaryData = {
